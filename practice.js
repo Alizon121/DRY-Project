4ff9fc65bbc771f486e00fbec27511b00fab2492
Andrew <andrew.lizon12@gmail.com>
2024-04-24 12:24:17 -0700

    Make helper function

--- practice.js
+++ practice.js
@@ -1,53 +1,73 @@
-function multiplyBiggerNumByTwo(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum * 2;
-  } else {
-    bigNum = num2;
-    return bigNum * 2;
-  }
+// Make a helper funciton that finds the biggest number of two numbers
+
+function findBigNum(num1, num2) {
+let bigNum;
+if (num1 > num2) {
+ return bigNum = num1;
+} else {
+  return bigNum = num2;
+}
 }
 
-function divideBiggerNumByThree(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum / 3;
-  } else {
-    bigNum = num2;
-    return bigNum / 3;
-  }
-}
 
-function eatMostTacos(sum1, sum2) {
-  let bigNum;
-  if (sum1 > sum2) {
-    bigNum = sum1;
-    return `I ate ${bigNum} tacos.`;
-  } else {
-    bigNum = sum2;
-    return `I ate ${bigNum} tacos.`;
-  }
-}
+// // Find the biggest number of two numbers and multiply it by two
+// function multiplyBiggerNumByTwo(num1, num2) {
+//   let bigNum;
+//   if (num1 > num2) {
+//     bigNum = num1;
+//     return bigNum * 2;
+//   } else {
+//     bigNum = num2;
+//     return bigNum * 2;
+//   }
+// }
 
-function adoptSmallerDog(weight1, weight2) {
-  let smallDog;
-  if (weight1 < weight2) {
-    smallDog = weight1;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  } else {
-    smallDog = weight2;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  }
-}
+
+
+// // Divides the bigggest number by three
+// function divideBiggerNumByThree(num1, num2) {
+//   let bigNum;
+//   if (num1 > num2) {
+//     bigNum = num1;
+//     return bigNum / 3;
+//   } else {
+//     bigNum = num2;
+//     return bigNum / 3;
+//   }
+// }
+
+// // Find the biggest number to put into a string
+// function eatMostTacos(sum1, sum2) {
+//   let bigNum;
+//   if (sum1 > sum2) {
+//     bigNum = sum1;
+//     return `I ate ${bigNum} tacos.`;
+//   } else {
+//     bigNum = sum2;
+//     return `I ate ${bigNum} tacos.`;
+//   }
+// }
+
+
+// // Find the smallest number of the two num arguments
+// // When implementing the helper funciton, it will require refactoring.
+// function adoptSmallerDog(weight1, weight2) {
+//   let smallDog;
+//   if (weight1 < weight2) {
+//     smallDog = weight1;
+//     return `I adopted a dog that weighs ${smallDog} pounds.`;
+//   } else {
+//     smallDog = weight2;
+//     return `I adopted a dog that weighs ${smallDog} pounds.`;
+//   }
+// }
 
 
 /**************************************************************************/
 /* DO NOT CHANGE THE CODE BELOW */
-module.exports = {
-  multiplyBiggerNumByTwo,
-  divideBiggerNumByThree,
-  eatMostTacos,
-  adoptSmallerDog
-};
+// module.exports = {
+//   multiplyBiggerNumByTwo,
+//   divideBiggerNumByThree,
+//   eatMostTacos,
+//   adoptSmallerDog
+// };
