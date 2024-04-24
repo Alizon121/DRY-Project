4e9adb83950caeb13a99ddf8be101cc75046b751
Andrew <andrew.lizon12@gmail.com>
2024-04-24 12:37:40 -0700

    Refactored Funcitons with One Helper Function

--- practice.js
+++ practice.js
@@ -17,42 +17,26 @@ function multiplyBiggerNumByTwo(num1, num2) {
 
 
 // // Divides the bigggest number by three
-// function divideBiggerNumByThree(num1, num2) {
-//   let bigNum;
-//   if (num1 > num2) {
-//     bigNum = num1;
-//     return bigNum / 3;
-//   } else {
-//     bigNum = num2;
-//     return bigNum / 3;
-//   }
-// }
+function divideBiggerNumByThree(num1, num2) {
+ return findBigNum(num1, num2)/3
+}
 
-// // Find the biggest number to put into a string
-// function eatMostTacos(sum1, sum2) {
-//   let bigNum;
-//   if (sum1 > sum2) {
-//     bigNum = sum1;
-//     return `I ate ${bigNum} tacos.`;
-//   } else {
-//     bigNum = sum2;
-//     return `I ate ${bigNum} tacos.`;
-//   }
-// }
+// Find the biggest number to put into a string
+function eatMostTacos(sum1, sum2) {
+    return `I ate ${findBigNum(sum1, sum2)} tacos.`;
+}
 
 
 // // Find the smallest number of the two num arguments
 // // When implementing the helper funciton, it will require refactoring.
-// function adoptSmallerDog(weight1, weight2) {
-//   let smallDog;
-//   if (weight1 < weight2) {
-//     smallDog = weight1;
-//     return `I adopted a dog that weighs ${smallDog} pounds.`;
-//   } else {
-//     smallDog = weight2;
-//     return `I adopted a dog that weighs ${smallDog} pounds.`;
-//   }
-// }
+function adoptSmallerDog(weight1, weight2) {
+  if (findBigNum(weight1, weight2) > weight1) {
+    return `I adopted a dog that weighs ${(weight1)} pounds.`;
+  }  
+  else {
+    return `I adopted a dog that weighs ${(weight2)} pounds.`;
+  }
+}
 
 
 /**************************************************************************/
